fee909747d9b0a7584ee2a7c75ae740f11a09d98
Henry Derbyshire <henry@htderbyshire.com>
2022-05-05 13:04:37 +0100

    completed exercise 1

---
+++ chapter5/ex_1/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var cs1 = context.getBean("commentService", CommentService.class);
+        var cs2 = context.getBean("commentService", CommentService.class);
+
+        boolean isSameBean = cs1 == cs2;
+
+        System.out.println(isSameBean);
+    }
+}
