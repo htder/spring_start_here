ff95f04be26a31de1b01450f827f3676e4d58c48
Henry Derbyshire <henry@htderbyshire.com>
2022-05-09 12:27:55 +0100

    completed exercise 5

---
+++ chapter6/ex_5/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import models.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = context.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setText("Demo Comment!");
+        comment.setAuthor("Natasha");
+
+        service.publishComment(comment);
+        service.deleteComment(comment);
+        service.editComment(comment);
+    }
+}
