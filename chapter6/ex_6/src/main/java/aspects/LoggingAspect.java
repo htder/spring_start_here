c59b5fa3e3299700b310b23d4f71537d54f46972
Henry Derbyshire <henry@htderbyshire.com>
2022-05-09 13:24:03 +0100

    completed exercise 6

---
+++ chapter6/ex_6/src/main/java/aspects/LoggingAspect.java
@@ -0,0 +1,28 @@
+package aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around(value = "@annotation(ToLog)")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Logging Aspect: Calling the intercepted method");
+
+        Object returnedValue = joinPoint.proceed();
+
+        logger.info("Logging Aspect: Method executed and returned " + returnedValue);
+
+        return returnedValue;
+    }
+
+}
