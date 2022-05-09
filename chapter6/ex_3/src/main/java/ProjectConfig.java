94bbd49e1eb0bf5dbe3cc09bf511ba11c3b9ae91
Henry Derbyshire <henry@htderbyshire.com>
2022-05-09 11:17:21 +0100

    completed exercise 3

---
+++ chapter6/ex_3/src/main/java/ProjectConfig.java
@@ -0,0 +1,9 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = {"aspects", "models", "services"})
+@EnableAspectJAutoProxy
+public class ProjectConfig {
+}
