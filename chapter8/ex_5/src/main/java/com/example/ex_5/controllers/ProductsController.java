d50f918b1c748757b511892a69764ce6dbf727fb
Henry Derbyshire <henry@htderbyshire.com>
2022-05-11 11:56:21 +0100

    completed exercise 5

---
+++ chapter8/ex_5/src/main/java/com/example/ex_5/controllers/ProductsController.java
@@ -0,0 +1,46 @@
+package com.example.ex_5.controllers;
+
+import com.example.ex_5.models.Product;
+import com.example.ex_5.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductsController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductsController(
+            ProductService productService
+    ) {
+        this.productService = productService;
+    }
+
+    @GetMapping(path = "/products")
+    public String viewProducts(Model model) {
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
+
+    @PostMapping(path = "/products")
+    public String addProduct(
+            @RequestParam String name,
+            @RequestParam double price,
+            Model model
+    ) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+}
---
+++ chapter8/ex_5/src/main/java/com/example/ex_5/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.example.ex_5.services;
+
+import com.example.ex_5.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p) {
+        products.add(p);
+    }
+
+    public List<Product> findAll() {
+        return products;
+    }
+
+}
