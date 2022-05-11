6d0c5726594b4b2a9f23da7c9da1b2060b6188bf
Henry Derbyshire <henry@htderbyshire.com>
2022-05-11 14:29:08 +0100

    completed exercise 1

---
+++ chapter9/ex_1/src/main/java/com/example/ex_1/LoginController.java
@@ -0,0 +1,45 @@
+package com.example.ex_1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private final LoginProcessor loginProcessor;
+
+    @Autowired
+    public LoginController(
+            LoginProcessor loginProcessor
+    ) {
+        this.loginProcessor = loginProcessor;
+    }
+
+    @GetMapping("/")
+    public String loginGet() {
+        return "login.html";
+    }
+
+    @PostMapping("/")
+    public String loginPost(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model
+    ) {
+        loginProcessor.setUsername(username);
+        loginProcessor.setPassword(password);
+        boolean loggedIn = loginProcessor.login();
+
+        if (loggedIn) {
+            model.addAttribute("message", "You are now logged in");
+        } else {
+            model.addAttribute("message", "Login failed");
+        }
+        return "login.html";
+    }
+
+}
