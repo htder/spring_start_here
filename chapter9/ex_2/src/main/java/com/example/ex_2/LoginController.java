30e561ebb114723fc9e8da0d3ecfc8faa0fea903
Henry Derbyshire <henry@htderbyshire.com>
2022-05-11 14:48:30 +0100

    completed exercise 2

---
+++ chapter9/ex_2/src/main/java/com/example/ex_2/LoginController.java
@@ -0,0 +1,45 @@
+package com.example.ex_2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private final LoginProcessor loginProcessor;
+
+    @Autowired
+    public LoginController(
+            LoginProcessor loginProcessor
+    ) {
+        this.loginProcessor = loginProcessor;
+    }
+
+    @GetMapping("/")
+    public String loginGet() {
+        return "login";
+    }
+
+    @PostMapping("/")
+    public String loginPost(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model
+    ) {
+        loginProcessor.setUsername(username);
+        loginProcessor.setPassword(password);
+        boolean loggedIn = loginProcessor.login();
+
+        if (loggedIn) {
+            model.addAttribute("message", "You are now logged in");
+            return "redirect:/main";
+        }
+
+        model.addAttribute("message", "Login failed");
+        return "login";
+    }
+}
---
+++ chapter9/ex_2/src/main/java/com/example/ex_2/MainController.java
@@ -0,0 +1,39 @@
+package com.example.ex_2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+
+    @Autowired
+    public MainController(
+            LoggedUserManagementService loggedUserManagementService
+    ) {
+        this.loggedUserManagementService = loggedUserManagementService;
+    }
+
+    @GetMapping("/main")
+    public String home(
+            @RequestParam(required = false) String logout,
+            Model model
+    ) {
+        if (logout != null) {
+            loggedUserManagementService.setUsername(null);
+        }
+        String username = loggedUserManagementService.getUsername();
+
+        if (username == null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("username", username);
+        return "main.html";
+    }
+
+}
