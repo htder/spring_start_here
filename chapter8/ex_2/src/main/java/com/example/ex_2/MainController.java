7ac3564d756eb54f0f036dbef7b14fa7e7a1bfc9
Henry Derbyshire <henry@htderbyshire.com>
2022-05-11 10:47:10 +0100

    completed exercise 2

--- chapter8/ex_2/src/main/java/com/example/ex_2/MainController.java
+++ chapter8/ex_2/src/main/java/com/example/ex_2/MainController.java
@@ -1,4 +1,21 @@
 package com.example.ex_2;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+    @RequestMapping("/home")
+    public String home(
+            @RequestParam String color,
+            Model page
+    ) {
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+
 }
