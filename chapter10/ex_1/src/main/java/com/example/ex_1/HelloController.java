d791f22912b4749c3bda17529fc4c75db1913825
Henry Derbyshire <henry@htderbyshire.com>
2022-05-12 12:27:37 +0100

    completed exercise 1

---
+++ chapter10/ex_1/src/main/java/com/example/ex_1/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.ex_1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello!";
+    }
+
+    @GetMapping("/ciao")
+    @ResponseBody
+    public String ciao() {
+        return "Ciao!";
+    }
+
+}
