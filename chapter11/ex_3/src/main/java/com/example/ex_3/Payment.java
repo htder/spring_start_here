e7d5e9fad43dcf3979ba15d21bbb91ae395086fc
Henry Derbyshire <henry@htderbyshire.com>
2022-05-12 16:48:22 +0100

    completed exercise 3

---
+++ chapter11/ex_3/src/main/java/com/example/ex_3/Payment.java
@@ -0,0 +1,23 @@
+package com.example.ex_3;
+
+public class Payment {
+
+    private String id;
+    private int amount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ chapter11/ex_3/src/main/java/com/example/ex_3/PaymentsController.java
@@ -0,0 +1,29 @@
+package com.example.ex_3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentsController {
+
+    private final PaymentsProxy paymentsProxy;
+
+    @Autowired
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
+    }
+
+    @PostMapping("/payment")
+    public Mono<Payment> createpayment(
+            @RequestBody Payment payment
+    ) {
+        String requestId = UUID.randomUUID().toString();
+        return paymentsProxy.createPayment(requestId, payment);
+    }
+
+}
