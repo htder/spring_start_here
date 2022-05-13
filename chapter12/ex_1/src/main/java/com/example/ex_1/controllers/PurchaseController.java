c431085374041c7cbba536030082a95a053f72d4
Henry Derbyshire <henry@htderbyshire.com>
2022-05-13 12:07:20 +0100

    completed exercise 1

---
+++ chapter12/ex_1/src/main/java/com/example/ex_1/controllers/PurchaseController.java
@@ -0,0 +1,33 @@
+package com.example.ex_1.controllers;
+
+import com.example.ex_1.models.Purchase;
+import com.example.ex_1.repositories.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    @Autowired
+    public PurchaseController(
+            PurchaseRepository purchaseRepository
+    ) {
+        this.purchaseRepository = purchaseRepository;
+    };
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+
+}
